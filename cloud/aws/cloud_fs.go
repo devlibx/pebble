8465540be8c7bd4040b7e007e1c6b44bde39e51b
Harish Bohara <harish.bohara@gmail.com>
2023-03-23 14:14:27 +0530

    [*] starting cloud fs - using *CloudFS instead of CloudFS

--- cloud/aws/cloud_fs.go
+++ cloud/aws/cloud_fs.go
@@ -10,82 +10,82 @@ type CloudFS struct {
 	wrapperFs vfs.FS
 }
 
-func (c CloudFS) Create(name string) (vfs.File, error) {
+func (c *CloudFS) Create(name string) (vfs.File, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) Link(oldname, newname string) error {
+func (c *CloudFS) Link(oldname, newname string) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) Open(name string, opts ...vfs.OpenOption) (vfs.File, error) {
+func (c *CloudFS) Open(name string, opts ...vfs.OpenOption) (vfs.File, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) OpenDir(name string) (vfs.File, error) {
+func (c *CloudFS) OpenDir(name string) (vfs.File, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) Remove(name string) error {
+func (c *CloudFS) Remove(name string) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) RemoveAll(name string) error {
+func (c *CloudFS) RemoveAll(name string) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) Rename(oldname, newname string) error {
+func (c *CloudFS) Rename(oldname, newname string) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) ReuseForWrite(oldname, newname string) (vfs.File, error) {
+func (c *CloudFS) ReuseForWrite(oldname, newname string) (vfs.File, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) MkdirAll(dir string, perm os.FileMode) error {
+func (c *CloudFS) MkdirAll(dir string, perm os.FileMode) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) Lock(name string) (io.Closer, error) {
+func (c *CloudFS) Lock(name string) (io.Closer, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) List(dir string) ([]string, error) {
+func (c *CloudFS) List(dir string) ([]string, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) Stat(name string) (os.FileInfo, error) {
+func (c *CloudFS) Stat(name string) (os.FileInfo, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) PathBase(path string) string {
+func (c *CloudFS) PathBase(path string) string {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) PathJoin(elem ...string) string {
+func (c *CloudFS) PathJoin(elem ...string) string {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) PathDir(path string) string {
+func (c *CloudFS) PathDir(path string) string {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c CloudFS) GetDiskUsage(path string) (vfs.DiskUsage, error) {
+func (c *CloudFS) GetDiskUsage(path string) (vfs.DiskUsage, error) {
 	//TODO implement me
 	panic("implement me")
 }
